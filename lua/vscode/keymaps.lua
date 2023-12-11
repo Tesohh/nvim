44ea549583e76a770a61d069795a2df467e742fd
Simone Tesini <simozilla006@gmail.com>
2023-12-11 21:55:04 +0100

    keymapp

--- lua/vscode/keymaps.lua
+++ lua/vscode/keymaps.lua
@@ -19,6 +19,7 @@ vskeymap('n', '<leader>ca', "editor.action.quickFix")
 vskeymap('n', '<leader>cl', "codelens.showLensesInCurrentLine")
 vskeymap('n', '<leader>n', "codelens.showLensesInCurrentLine")
 vskeymap('n', '<leader>ra', "editor.action.rename")
+vskeymap({'n', 'x'}, '<leader>rr', "rest-client.request")
 
 -- fold support
 vskeymap('n', 'zM', 'editor.foldAll')
@@ -28,16 +29,21 @@ vskeymap('n', 'zC', 'editor.foldRecursively')
 vskeymap('n', 'zo', 'editor.unfold')
 vskeymap('n', 'zO', 'editor.unfoldRecursively')
 vskeymap('n', 'za', 'editor.toggleFold')
-vskeymap({'n', 'x'}, 'j', 'cursorDown')
-vskeymap({'n', 'x'}, 'k', 'cursorUp')
+vskeymap({'n'}, 'j', 'cursorDown')
+vskeymap({'n'}, 'k', 'cursorUp')
 
 -- ui
 vskeymap({'n', 'x'}, '<leader>e', 'workbench.files.action.focusFilesExplorer')
 vskeymap({'n', 'x'}, '<leader>t', 'terminal.focus')
 
 -- searching
-vskeymap({'n', 'x'}, '<leader>ff', 'workbench.action.quickOpen')
-vskeymap({'n', 'x'}, ';', 'workbench.action.showCommands')
+vskeymap({'n', 'x'}, '<leader>f', 'workbench.action.quickOpen')
+vskeymap({'n', 'x'}, ';', 'workbench.action.showEditorsInActiveGroup')
+-- vskeymap({'n', 'x'}, '', 'workbench.action.showCommands')
 vskeymap({'n', 'x'}, '<leader><leader>', 'workbench.action.showCommands')
 
 -- debugging
+
+-- buffer
+vskeymap({'n','x'}, 'H', "workbench.action.previousEditor")
+vskeymap({'n','x'}, 'L', "workbench.action.nextEditor")
