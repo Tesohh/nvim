433acec72866e3e46efda56455c0b4770279e41a
Simone Tesini <simozilla006@gmail.com>
2023-12-31 01:18:21 +0100

    add GitC

--- lua/nv/plugins/gitsigns.lua
+++ lua/nv/plugins/gitsigns.lua
@@ -4,5 +4,15 @@ return {
     config = function()
         require("gitsigns").setup()
         vim.keymap.set({ "n", "x" }, "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>")
+        vim.api.nvim_create_user_command("GitC", function(cmd)
+            local msg = ""
+            if cmd.args then
+                msg = cmd.args
+            else
+                msg = vim.fn.input("commit message > ")
+            end
+
+            vim.cmd('!git add .; git commit -m "' .. msg .. '"; git push')
+        end, { nargs = 1 })
     end,
 }
