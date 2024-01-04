e8b3d1f2059e4a86fbcb89572366c64f13ba7277
Simone Tesini <simozilla006@gmail.com>
2024-01-04 20:43:06 +0100

    add harpoon in lualine

--- lua/nv/plugins/lualine.lua
+++ lua/nv/plugins/lualine.lua
@@ -1,31 +1,59 @@
+local function basename(str)
+	return string.gsub(str, "(.*/)(.*)", "%2")
+end
+
+local function lualine_harpoon()
+	local harpoon = require("harpoon")
+	local keys = { "a", "s", "d", "f" }
+	local currentfile = basename(vim.fn.expand("%:p"))
+
+	local s = ""
+
+	for i, v in ipairs(harpoon:list().items) do
+		local fn = basename(v.value)
+		local prefix = fn ~= currentfile and keys[i] or "󰛢"
+
+		s = s .. prefix .. " " .. fn
+		if i < #harpoon:list().items then
+			s = s .. " ► "
+		end
+	end
+
+	if #harpoon:list().items == 0 then
+		s = "󰛢"
+	end
+
+	return s
+end
+
 return {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-        options = {
-            component_separators = "|",
-            section_separators = { left = "", right = "" },
-        },
-        sections = {
-            lualine_a = {
-                { "mode", separator = { left = "" }, right_padding = 2 },
-            },
-            lualine_b = { "filename", "branch", "diff" },
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = { "progress" },
-            lualine_z = {
-                { "location", separator = { right = "" }, left_padding = 2 },
-            },
-        },
-        inactive_sections = {
-            lualine_a = { "filename" },
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = { "location" },
-        },
-        tabline = {},
-        extensions = {},
-    },
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		options = {
+			component_separators = "|",
+			section_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_a = {
+				{ "mode", separator = { left = "" }, right_padding = 2 },
+			},
+			lualine_b = { "filename", "branch", "diff" },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = { { lualine_harpoon } },
+			lualine_z = {
+				{ "location", separator = { right = "" }, left_padding = 2 },
+			},
+		},
+		inactive_sections = {
+			lualine_a = { "filename" },
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = { "location" },
+		},
+		tabline = {},
+		extensions = {},
+	},
 }
--- lua/nv/plugins/telescope.lua
+++ lua/nv/plugins/telescope.lua
@@ -1,20 +1,20 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            vim.keymap.set({ "n", "x" }, "<leader>f", "<cmd>Telescope find_files theme=dropdown<cr>")
-            vim.keymap.set({ "n", "x" }, ";", "<cmd>Telescope buffers theme=dropdown<cr>")
-            vim.keymap.set({ "n", "x" }, "<leader>sw", "<cmd>Telescope live_grep<cr>")
-            vim.keymap.set({ "n", "x" }, "<leader>ss", "<cmd>Telescope lsp_document_symbols theme=ivy<cr>")
-            vim.keymap.set({ "n", "x" }, "<leader>sS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>")
-            vim.keymap.set({ "n", "x" }, "<leader>se", "<cmd>Telescope symbols theme=ivy<cr>")
-            vim.keymap.set({ "n", "x" }, "<leader>gc", "<cmd>Telescope git_commits<cr>")
-            vim.keymap.set({ "n", "x" }, "<leader>gs", "<cmd>Telescope git_status<cr>")
-        end,
-    },
-    {
-        "nvim-telescope/telescope-symbols.nvim",
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			vim.keymap.set({ "n", "x" }, "<leader>f", "<cmd>Telescope find_files theme=dropdown<cr>")
+			-- vim.keymap.set({ "n", "x" }, ";", "<cmd>Telescope buffers theme=dropdown<cr>")
+			vim.keymap.set({ "n", "x" }, "<leader>sw", "<cmd>Telescope live_grep<cr>")
+			vim.keymap.set({ "n", "x" }, "<leader>ss", "<cmd>Telescope lsp_document_symbols theme=ivy<cr>")
+			vim.keymap.set({ "n", "x" }, "<leader>sS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>")
+			vim.keymap.set({ "n", "x" }, "<leader>se", "<cmd>Telescope symbols theme=ivy<cr>")
+			vim.keymap.set({ "n", "x" }, "<leader>gc", "<cmd>Telescope git_commits<cr>")
+			vim.keymap.set({ "n", "x" }, "<leader>gs", "<cmd>Telescope git_status<cr>")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-symbols.nvim",
+	},
 }
